fcf3fea34b8f14274df55107dd5b896b035d7473
Sidharth <psidharth2005@gmail.com>
2026-02-07 21:10:09 +0530

    Fix MongoDB connection and verify backend working

--- server/index.js
+++ server/index.js
@@ -1,12 +1,33 @@
+const fs = require("fs");
+const path = require("path");
+
+
+
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
+const mongoose = require("mongoose");
+//require("dotenv").config({ path: __dirname + "/.env" });
 
 const app = express();
+const expenseRoutes = require("./routes/expenseRoutes");
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+// MongoDB connection (THIS WAS MISSING)
+mongoose
+  .connect(
+    "mongodb+srv://Expense_user:Murivalan%402005@m0.lv4pnz1.mongodb.net/expense-tracker?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+
+// Routes
+app.use("/api/expenses", expenseRoutes);
 
 // Test route
 app.get("/", (req, res) => {
